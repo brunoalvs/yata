ebdcd5be0bf5eebc5723ebe81f152ffd0b63b9ae
Bruno Alves <hello.brunoalves@gmail.com>
2019-12-05 13:10:53 -0300

    nao sei oq é

---
+++ components/Templates/Login/index.jsx
@@ -0,0 +1,15 @@
+function LoginTemplate () {
+	return (
+		<section class='login-template'>
+			<h1>Logo</h1>
+			<form>
+				<label for='userid'>
+					Login:
+				</label>
+				<input type='text' />
+			</form>
+		</section>
+	)
+}
+
+export default LoginTemplate
