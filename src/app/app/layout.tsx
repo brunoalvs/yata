68f60c37d5f53c9d9e671ca03c470eb7116c66c0
Bruno Alves <hello@brunoalves.app>
2023-03-31 07:28:34 +0100

    basic layout app included

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,16 +1,48 @@
+'use client'
+import { useEffect, useState } from 'react'
+import styles from './styles.module.css'
+
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export default function Layout ({ children }: LayoutProps) {
+  const [isMobile, setIsMobile] = useState(false)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 768)
+
+    return () => {
+      setIsMobile(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div className='grid grid-cols-layout-app gap-2 dark:text-white'>
-      <aside className='border border-red-400 sm:fixed sm:top-0 sm:left-0'>
+    <div className={ styles.container }>
+      <aside className={ styles.sidebar }>
         <h1>Aside</h1>
       </aside>
-      <div>
+      <div className={ styles.content }>
+        { isMobile && <button onClick={ () => setIsDrawerOpen(!isDrawerOpen) }>Toggle</button> }
         { children }
+        <button onClick={ () => setIsDrawerOpen(!isDrawerOpen) }>Toggle</button>
       </div>
+      <section className={ styles.drawer } data-isOpen={ isDrawerOpen }>
+        <h1>Section</h1>
+      </section>
     </div>
   )
 }
