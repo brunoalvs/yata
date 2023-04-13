74b6ebd2e33637eb1500fe38a5110d4a0bb0392c
Bruno Alves <hello@brunoalves.app>
2023-04-13 20:11:35 +0100

    create all tasks page and refactor sidebar component of layout

---
+++ src/components/common/index.tsx
@@ -0,0 +1,5 @@
+import { Button } from './Button'
+import { ButtonIcon } from './ButtonIcon'
+import { HeadingPage } from './HeadingPage'
+
+export { Button, ButtonIcon, HeadingPage }
--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,11 +1,11 @@
 import { useContext } from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { FiPlus, FiPlusSquare, FiMenu } from 'react-icons/fi'
 
 import { TaskContext } from '@/contexts/task'
-import { FiPlus, FiPlusSquare, FiMenu } from 'react-icons/fi'
+import { Button, ButtonIcon } from '@/components/common'
 import { SidebarUserOptions } from '@/components/SidebarUserOptions'
-import { Button } from '@/components/common/Button'
-import { ButtonIcon } from '@/components/common/ButtonIcon'
 import styles from './styles.module.scss'
 
 interface SidebarProps {
@@ -15,30 +15,48 @@ interface SidebarProps {
 
 export const Sidebar = ({ isOpen, closeSidebar }: SidebarProps) => {
   const { collections } = useContext(TaskContext)
+  const pathname = usePathname()
 
   return (
     <aside className={ styles.container } data-open={ isOpen }>
-      <header>
+      <header className={ styles.header }>
         <ButtonIcon onClick={ () => closeSidebar() } title='Close Sidebar'>
           <FiMenu />
         </ButtonIcon>
         <SidebarUserOptions />
       </header>
-      <section>
+      <section className={ styles.content }>
         <nav className={ `${styles.lists} ${styles.default}` }>
-          <Link href='/app' title='A list that refreshes every day'>
+          <Link
+            href='/app'
+            title='A list that refreshes every day'
+            data-current={ pathname === '/app' }
+          >
             Today
           </Link>
+          <Link
+            href='/app/all'
+            title='All tasks'
+            data-current={ pathname === '/app/all' }
+          >
+            All
+          </Link>
+        </nav>
+        <nav className={ styles.lists }>
           {
             collections.collections.map((collection) => (
-              <Link key={ collection.id } href={ `app/collection/${collection.id}` }>
+              <Link
+                key={ collection.id }
+                href={ `/app/collection/${collection.id}` }
+                data-current={ pathname?.includes(collection.id) ? 'true' : 'false' }
+              >
                 { collection.title }
               </Link>
             ))
           }
         </nav>
       </section>
-      <footer>
+      <footer className={ styles.footer }>
         <Button
           className={ styles.buttonNewCollection }
           onClick={ () => collections.addCollection('Untitled collection') }
