de5876993db4bdfec9c4bcea68e5f8c7d9f443d8
Bruno Alves <hello@brunoalves.app>
2023-04-15 03:52:24 +0100

    homepage refactored

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Button, HeadingPage } from '@/components/common'
 import { Logo } from '@/components/Logo'
 import styles from './styles.module.scss'
+import BackgroundAnimation from '@/components/BackgroundAnimation'
 
 export default function HomeTemplate () {
   return (
@@ -31,6 +32,7 @@ export default function HomeTemplate () {
             <p>
               Yata is a web application that allows you to manage your tasks in a simple and intuitive way.
             </p>
+            <p>Stop wasting time and start achieving your goals with our Todo App. Sign up today and experience the power of efficient task management!</p>
             <Link href='/app'>
               <Button className={ styles.buttonGetStarted }>
                 Get Started
@@ -38,7 +40,7 @@ export default function HomeTemplate () {
             </Link>
           </section>
         </div>
-        <div className={ styles.backgroundAnimate } />
+        <BackgroundAnimation />
       </div>
     </>
   )
