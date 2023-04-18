306b56d8c1c26d34cd656b53c4ab206e8cea83ff
Bruno Alves <hello@brunoalves.app>
2023-04-19 00:28:12 +0100

    adding layout app

--- src/pages/app/all.tsx
+++ src/pages/app/all.tsx
@@ -3,19 +3,6 @@ import { useRouter } from 'next/router'
 import type { NextPageWithLayout } from '../_app'
 import App from '@/layouts/App'
 import { TaskProvider } from '@/contexts/task'
-import { FiPlus } from 'react-icons/fi'
-
-const styles = {
-  container: {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: 'xxx-large'
-  } as React.CSSProperties,
-}
 
 const AppPage: NextPageWithLayout = () => {
   const { status } = useSession()
@@ -23,21 +10,19 @@ const AppPage: NextPageWithLayout = () => {
 
   if (status === 'loading') {
     return (
-      <div style={ styles.container }>
+      <div>
         <h1>Loading...</h1>
       </div>
     )
   }
 
   if (status === 'unauthenticated') {
-
     setTimeout(() => {
       push('/signin')
     }, 3000)
 
     return (
-      <div
-        style={ styles.container }>
+      <div>
         <p>You are not signed in.</p>
         <p>Redirecting to Sign In...</p>
       </div>
@@ -45,15 +30,9 @@ const AppPage: NextPageWithLayout = () => {
   }
 
   return (
-    <div style={ styles.container }>
-      <h1>App Page</h1>
+    <div>
+      <h1>All Tasks</h1>
       <p>When user is Logged!</p>
-      <label
-        style={{ display: 'flex', backgroundColor: 'white', padding: '1rem', borderRadius: '0.5rem' }}
-      >
-        <FiPlus />
-        <input type='text' placeholder='Add a task' />
-      </label>
     </div>
   )
 }
