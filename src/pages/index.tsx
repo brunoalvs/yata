733e97f24011186cce158642dcc0b6d4b3042c50
Bruno Alves <hello@brunoalves.app>
2023-05-15 12:48:29 +0100

    app screenshot is now commented, to avoid shows the incorrect image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPageWithLayout } from './_app'
 import { useRouter } from 'next/router'
-import Image from 'next/image'
+// import Image from 'next/image'
 
-import screenshotExample from '../../public/screenshot-example.webp'
+// import screenshotExample from '../../public/screenshot-example.webp'
 import Institutional from '@/layouts/Institutional'
 import HeadingPage from '@/components/atoms/HeadingPage'
 import Button from '@/components/atoms/Button'
@@ -19,13 +19,13 @@ const IndexPage: NextPageWithLayout = () => {
         Get Started
       </Button>
 
-      <figure>
+      { /* <figure>
         <Image
           src={ screenshotExample }
           alt='Yata App Screenshot'
           style={{ width: '100%', height: 'auto', objectFit: 'contain', margin: '2.4rem 0', borderRadius: '0.5rem', overflow: 'hidden' }}
         />
-      </figure>
+      </figure> */ }
     </>
   )}
 
