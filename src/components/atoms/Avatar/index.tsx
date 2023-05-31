47da9f6f56ae7c4e6a771a73e705e29add67a674
Bruno Alves <hello.brunoalves@gmail.com>
2023-05-31 21:33:24 +0100

    cleaning up the Avatar atomic component

--- src/components/atoms/Avatar/index.tsx
+++ src/components/atoms/Avatar/index.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState, useCallback, memo } from 'react'
+import { Suspense, memo } from 'react'
 import Image from 'next/image'
 
 import * as S from './styles'
@@ -16,42 +16,14 @@ interface AvatarProps {
 }
 
 const Avatar = ({ src, name, size = 'medium' }: AvatarProps) => {
-  const [imageSrc, setSrc] = useState<string>(src)
-
-  function getName(name: string) {
-    const [firstName, lastName] = name.split(' ')
-    return `${firstName}+${lastName}`
-  }
-
-  const handleImageError = useCallback(() => {
-    setSrc(`https://ui-avatars.com/api/?name=${getName(name)}}`)
-    console.log('error')
-  }, [name])
-
-  console.log('userPhotoURL: ', {
-    imageSrc: {
-      src: imageSrc,
-      type: typeof imageSrc,
-    },
-    src: {
-      src,
-      type: typeof src,
-    },
-  })
-
   return (
     <S.Container>
       <Suspense fallback={<div>loading...</div>}>
         <Image
-          src={
-            imageSrc === ''
-              ? `https://ui-avatars.com/api/?name=${getName(name)}}`
-              : imageSrc
-          }
+          src={src === '' ? `https://ui-avatars.com/api/?name=${name}}` : src}
           alt={`Avatar of ${name}`}
           width={AvatarSize[size]}
           height={AvatarSize[size]}
-          onError={handleImageError}
         />
       </Suspense>
     </S.Container>
